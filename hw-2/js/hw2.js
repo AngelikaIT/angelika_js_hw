687288b3948acc0489346f31bf623e63b398f66d
AngelikaIT <anzhelikamalysh21@gmail.com>
2021-10-23 13:18:40 +0300

    create hw-2

---
+++ hw-2/js/hw2.js
@@ -0,0 +1,24 @@
+// Создайте три переменных для хранения возраста, имени и фамилии пользователя. Инициализируйте ее
+
+let user_age = 30;
+const user_name = "Angelika";
+let user_surname = "Vasilenko";
+
+// переменной, которая хранит нового пользователя
+// переменной, которая храненит список покупок
+// переменной, которая хранит информацию о том, является ли пользователь в сети (online)
+// переменной, которая хранит зарплату работника
+// переменной, которая хранит координаты курсора (мыши)
+
+let user_new;
+let user_shoppingList;
+let user_online;
+let user_salary; 
+// let employeeSalary
+let cursor_x;
+let cursor_y;
+// let mouseCoordinates;
+
+console.log(user_age);
+console.log("surname",user_surname);
+console.log("age", typeof user_age)
