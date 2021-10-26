8480b3e3e126309b759d1c4e812299146817251d
AngelikaIT <anzhelikamalysh21@gmail.com>
2021-10-26 19:20:56 +0300

    create hw-3

---
+++ hw-3/js/1_.js
@@ -0,0 +1,16 @@
+// ###Задание 1
+
+// Спросить с помощью prompt у пользователя 2 числа и записать их в переменные.
+// Сравнить полученные числа.
+// С помощью alert вывести на экран сообщение о том, какое число больше. Если числа равны - сообщить что числа равны
+
+// const userName =  prompt("Hello! What is your name?");
+// const number_1 =  +prompt(userName + ", enter first number, please:");
+// const number_2 =  +prompt (userName + ", enter second number, please:");
+
+// if (number_1 > number_2) {
+//     alert (number_1 + " more than " + number_2);
+// } else if (number_1 < number_2) {
+//     alert (number_1 + " less than " + number_2);
+// } else 
+//     alert (number_1 + " equals " + number_2);
---
+++ hw-3/js/2_.js
@@ -0,0 +1,28 @@
+// Задание 2
+
+// Реализовать калькулятор используя prompt и alert.
+
+// Последовательно спросить у пользователя два числа и желаемое действие.
+// Действия могут быть следующие: + - / *
+// Выполнить математическую операцию с полученными значениями.
+// Результат показать пользователю
+// Подсказка: В данном задании используйте оператор switch case
+
+// const userName   =  prompt("Hello! What is your name?");
+// const number_1   = +prompt(userName + ", enter first number, please:");
+// const number_2   = +prompt (userName + ", enter second number, please:");
+// const mathAction =  prompt (userName + ", enter mathematical action (+ - * /), please:");
+
+// switch (mathAction) {
+//     case "+":
+//         alert(number_1 + number_2);
+//         break;
+//     case "-":
+//         alert(number_1 - number_2);
+//         break;
+//     case "/":
+//         alert(number_1 / number_2);
+//         break;
+//     default:
+//         alert(number_1 * number_2);
+// }
